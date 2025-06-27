48be96e286de66476966a6b5eb3c64dad0e0ebb4
aman78099 <amanrawat@riccarton.school.nz>
2025-06-27 21:13:10 +1200

    Update Search-page.js

--- Search-page.js
+++ Search-page.js
@@ -1,3 +1,6 @@
+// This JavaScript function creates a live search feature. As the user types in the search input field,
+// it filters through a list of items and only shows the ones that match the search text, 
+// hiding all others in real time.
 function myFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("mySearch");
